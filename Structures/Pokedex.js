a8ca12292599e58d81bfb90e8331859c10fa509a
ACANTELOUP <anthony.canteloup@ynov.com>
2024-08-19 12:22:50 +0200

    TypoFix - Forgot to edit ability for Exeggutor

--- Structures/Pokedex.js
+++ Structures/Pokedex.js
@@ -87,7 +87,7 @@ export const Pokedex = {
         Image: '../Images_Pokemon/Noadkoko.png',
         Capacites: [
             Capacites.MEGAFOUET,
-            Capacites.PSYCHO,
+            Capacites.PSYKO,
             Capacites.DAMOCLES,
             Capacites.POUVOIR_ANTIQUE,
         ]
